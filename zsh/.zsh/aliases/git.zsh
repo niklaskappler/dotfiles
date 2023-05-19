966884059d93897f8a5cccf5a579e258a1a701ba
Niklas Kappler <mail@niklas-kappler.de>
2023-05-19 14:09:31 +0200

    feat: add handy git-clean-merged command for removing local branches without any untracked remote

--- zsh/.zsh/aliases/git.zsh
+++ zsh/.zsh/aliases/git.zsh
@@ -2,4 +2,5 @@
 alias g=git
 # prettier & colorfull git log
 alias gitlog="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-
+# clean merged branches
+#alias git-clean-merged ='git fetch -p && for branch in $(git branch -vv | grep ": gone]" | awk "{print $1}"); do git branch -D $branch; done'
